980fcdf85dbc10c2007951b0943a83011b9d633e
olumuyiwaa <oladoyinemmanuel@gmail.com>
2025-06-19 12:17:21 +0100

    logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'Migrantifly - Your Migration Journey Starts Here',
   description: 'From dreaming to thriving - we guide you through every step of your migration journey.',
+   icons: {
+      icon: '/logo/logo.png',
+    },
 }
 
 export default function RootLayout({ children }) {
