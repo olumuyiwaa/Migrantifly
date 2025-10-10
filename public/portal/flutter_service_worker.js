a4d9423bf25b15e245a4e7e86e313d066770aec3
olumuyiwaa <oladoyinemmanuel@gmail.com>
2025-10-10 13:11:31 +0100

    portal improvement

--- public/portal/flutter_service_worker.js
+++ public/portal/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9c2ee21e445898b17dc9805c7b01be1f",
+const RESOURCES = {"flutter_bootstrap.js": "bb513844f560dcdf1c9944b4e7a776d8",
 "version.json": "f3ce942f3eb1bc582d4d237a5949d0de",
 "index.html": "235303d4e9ec7f42c1214b17facbc714",
 "/": "235303d4e9ec7f42c1214b17facbc714",
-"main.dart.js": "b9c0ec1183e200ee1357d57a3adcfa1e",
+"main.dart.js": "efecc26f91799afa60082ff9bb284f17",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
