3b9f568d2d28f7f8974a1f349dfe2d4f9957db11
olumuyiwaa <oladoyinemmanuel@gmail.com>
2025-10-16 14:54:07 +0100

    portal improvement

--- public/portal/flutter_bootstrap.js
+++ public/portal/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2147772981"
+    serviceWorkerVersion: "3118509615"
   }
 });
