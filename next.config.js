6c931b9748d35a35569c79659f52e4ecbca63cb8
olumuyiwaa <oladoyinemmanuel@gmail.com>
2025-11-03 20:38:42 +0100

    render deploy

--- next.config.js
+++ next.config.js
@@ -1,21 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
+  images: { domains: ['res.cloudinary.com'] },
+  output: 'standalone',
   async rewrites() {
     return [
-      {
-        source: '/portal',
-        destination: '/portal/index.html',
-      },
-      {
-        source: '/portal/:path*',
-        destination: '/portal/index.html',
-      },
+      { source: '/portal', destination: '/portal/index.html' },
+      { source: '/portal/:path*', destination: '/portal/index.html' }
     ];
-  },
+  }
 };
 
 export default nextConfig;
